7d81734d836e4b09309319fb71250cc6480c70ec
f04041 <06kisoukyoku@gmail.com>
2014-05-21 23:08:50 +0900

    first commit

---
+++ mods/blocksample/common/SampleBlockCore.java
@@ -0,0 +1,39 @@
+﻿package mods.blocksample.common;
+
+import mods.blocksample.common.block.BlockSample;
+import mods.blocksample.common.block.BlocksamplePanel;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
+@Mod(modid="BlockSampleMod", name="BlockSampleMod", version="1.0")
+public class SampleBlockCore
+{
+	public static Block blockSample;
+	public static int blockSampleID = 4087;
+	public static Block blockSamplePanel=(new BlocksamplePanel(blockSampleID+1, "iron_bars", "iron_bars", Material.wood, true)).setHardness(0.0F).setResistance(2000.0F).setStepSound(Block.soundStoneFootstep).setUnlocalizedName("blockSamplePanel");
+
+	@EventHandler
+	public void init(FMLInitializationEvent event)
+	{
+		//表示名の登録
+		LanguageRegistry.addName(blockSample, "Sample Block");
+		LanguageRegistry.instance().addNameForObject(blockSample, "ja_JP", "サンプル ブロック");
+		LanguageRegistry.addName(blockSamplePanel, "Sample Block Panel");
+		LanguageRegistry.instance().addNameForObject(blockSamplePanel, "ja_JP", "サンプル ブロックパネル");
+	}
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		//ブロックの登録
+		blockSample = new BlockSample(blockSampleID, Material.cloth);
+		GameRegistry.registerBlock(blockSample, "blockSample");
+		GameRegistry.registerBlock(blockSamplePanel, "blockSamplePanel");
+	}
+}
---
+++ mods/blocksample/common/block/BlockSample.java
@@ -0,0 +1,54 @@
+﻿package mods.blocksample.common.block;
+ 
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+ 
+public class BlockSample extends Block
+{
+ 
+	public BlockSample(int par1, Material par2Material)
+	{
+		super(par1, par2Material);
+		this.setCreativeTab(CreativeTabs.tabBlock);//クリエイティブタブの選択
+		this.setUnlocalizedName("blockSample");	//システム名の設定
+		this.setTextureName("samplemod:block_sample");	//ブロックのテクスチャの指定
+		//いらないものは消してください
+		this.setHardness(0.0F);			//硬さ
+		this.setResistance(2000.0F);		//爆破耐性
+		this.setStepSound(Block.soundStoneFootstep);	//ブロックの音
+		this.setLightValue(1.0F);	//明るさ 1.0F = 15
+		this.setBurnProperties(this.blockID, 5, 10);
+	}
+ 
+	/*
+	 * 以下おまけ
+	 * 特に機能がなければ消してしまってください
+	 */
+ 
+	@Override
+	public int idDropped(int metadata, Random rand, int fortune)
+	{
+		//ブロックを壊した時にドロップするもの
+		return this.blockID;
+	}
+	
+	@Override
+	public int quantityDropped(Random rand)
+	{
+		//ブロックを壊した時にドロップする量
+		return 1;
+	}
+ 
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float disX, float disY, float disZ)
+	{
+		//ブロックが右クリックされた時
+		return false;
+	}
+ 
+}
